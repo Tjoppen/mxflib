d105c15fa52375b44501dd300e73b9d6ba1dcd05
terabrit <terabrit>
2004-01-06 14:27:28 +0000

    BUG sense of anchor offset was inverted. revise diagnostic output

--- esp_mpeg2ves.cpp
+++ esp_mpeg2ves.cpp
@@ -1,7 +1,7 @@
 /*! \file	esp_mpeg2ves.cpp
  *	\brief	Implementation of class that handles parsing of MPEG-2 video elementary streams
  *
- *	\version $Id: esp_mpeg2ves.cpp,v 1.5 2003/12/18 17:51:55 matt-beard Exp $
+ *	\version $Id: esp_mpeg2ves.cpp,v 1.6 2004/01/06 14:27:28 terabrit Exp $
  *
  */
 /*
@@ -571,7 +571,7 @@ Uint64 MPEG2_VES_EssenceSubParser::ReadInternal(FileHandle InFile, Uint32 Stream
 						}
 
 						// Now we have determined if this is an anchor frame we can work out the anchor offset
-						int AnchorOffset = AnchorFrame - PictureNumber;
+						int AnchorOffset = PictureNumber - AnchorFrame;
 
 						//
 						// Offer this index table data to the index manager
@@ -579,6 +579,19 @@ Uint64 MPEG2_VES_EssenceSubParser::ReadInternal(FileHandle InFile, Uint32 Stream
 
 						Manager->OfferEditUnit(ManagedStreamID, PictureNumber, AnchorOffset, Flags);
 						Manager->OfferTemporalOffset(PictureNumber - (GOPOffset - TemporalReference), GOPOffset - TemporalReference);
+
+						// diagnostics
+						if(PictureNumber < 35)
+							printf( "  OfferEditUnit[%3d]: Tpres=%3d Aoff=%2d A=%3d 0x%02x. Reorder Toff[%2d]=%2d\n",
+											(int)PictureNumber,
+											(int)TemporalReference,
+											(int)AnchorOffset,
+											(int)AnchorFrame,
+											(int)Flags,
+                      (int)(PictureNumber - (GOPOffset - TemporalReference)),
+                      (int)(GOPOffset - TemporalReference)
+										 );
+
 					}
 
 					GOPOffset++;
@@ -590,7 +603,12 @@ Uint64 MPEG2_VES_EssenceSubParser::ReadInternal(FileHandle InFile, Uint32 Stream
 					BuffGetU8(InFile);
 					BuffGetU8(InFile);
 					BuffGetU8(InFile);
-					if(BuffGetU8(InFile) & 0x40) ClosedGOP = true; else ClosedGOP = false;
+
+					ClosedGOP = (BuffGetU8(InFile) & 0x40)? true:false;
+
+					//if( PictureNumber < 35 )
+					if( ClosedGOP ) printf( "Closed GOP\n" ); else printf( "Open GOP\n" );
+
 					CurrentPos += 4;
 				}
 				// Sequence header start code
