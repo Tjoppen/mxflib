5ce09f4e9d8a3256b9598620e192429c7c5f3e88
matt-beard <matt-beard>
2004-05-05 11:16:05 +0000

    Modified interface to support better KLV reading/writing

--- mxflib/klvobject.h
+++ mxflib/klvobject.h
@@ -3,7 +3,7 @@
  *
  *			Class KLVObject holds info about a KLV object
  *
- *	\version $Id: klvobject.h,v 1.1 2004/04/26 18:27:47 asuraparaju Exp $
+ *	\version $Id: klvobject.h,v 1.1.2.1 2004/05/05 11:16:05 matt-beard Exp $
  *
  */
 /*
@@ -91,6 +91,16 @@ namespace mxflib
 namespace mxflib
 {
 	//! KLV Object class
+	/*! This class gives access to single KLV items within an MXFfile.
+	 *  The normal use for this class is handling of essence data. Huge values can be safely 
+	 *  handled by loading them a "chunk" at a time.  Data is also available to identify the
+	 *  location of the value in an MXFFile so that MXFFile::Read() and MXFFile::Write can
+	 *  be used for efficient access.
+	 *  /note This class does <b>not</b> provide any interlock mechanism to ensure safe
+	 *        concurrent access. So if modified data is held in the object's DataChunk,
+	 *        but not yet written to the file, calls to KLVObject::ReadData() or
+	 *        MXFFile::Read() will return the <b>unmodifief</b> data.
+	 */
 	class KLVObject : public RefCount<KLVObject>
 	{
 	private:
@@ -110,7 +120,7 @@ namespace mxflib
 		~KLVObject() {};
 
 		//! Set the source details when an object has been read from a file
-		void SetSource(MXFFilePtr File, Uint64 Location, Uint32 NewKLSize)
+		void SetSource(MXFFilePtr File, Uint64 Location, Uint32 NewKLSize, Length ValueLen)
 		{
 			IsConstructed = false;
 			SourceOffset = Location;
@@ -119,7 +129,7 @@ namespace mxflib
 		}
 
 		//! Set the source details when an object has been read from memory
-		void SetSource(Uint64 Location, Uint32 NewKLSize)
+		void SetSource(Uint64 Location, Uint32 NewKLSize, Length ValueLen)
 		{
 			IsConstructed = false;
 			SourceOffset = Location;
@@ -139,11 +149,44 @@ namespace mxflib
 		//! Get text that describes where this item came from
 		std::string GetSource(void);
 
-		//! Get the size
-		Uint64 GetSize(void) { return KLSize; }
+		//! Get the size of the key and length (not of the value)
+		Uint64 GetKLSize(void) { return KLSize; }
 
 		//! Get a GCElementKind structure
 		GCElementKind GetGCElementKind(void);
+
+		//! Get the position of the first byte in the DataChunk as an offset into the file
+		/*! /return -1 if the data has not been read from a file (or the offset cannot be determined) 
+		 */
+		Position GetDataBase(void);
+
+		//! Set the position of the first byte in the DataChunk as an offset into the file
+		/*! /note This function must be used with great care as data may will be written to this location
+		 */
+		void SetDataBase(Position NewBase);
+
+		//! Read data from the KLVObject source into the DataChunk
+		/*! If Size is zero an attempt will be made to read all available data
+		 *  /note Any previously read data in the current DataChunk will be discarded before reading the new data
+		 */
+		Length ReadData(Position Start = 0, Length Size = 0);
+
+		//! Write data from the current DataChunk to the source file
+		/*! /note The data in the chunk will be written to the specified position 
+		 *  <B>regardless of the position from where it was origanally read</b>
+		 */
+		Length WriteData(Position Start = 0, Length Size = 0);
+
+		//! Function pointer type for KLVReader handlers
+		typedef Length (*KLVObjectReadHandler) (KLVObjectPtr Object, Position Start, Length Size );
+
+		//! Set a handler to supply data when a read is performed
+		/*! /note If not set it will be read from the source file (if available) or cause an error message
+		 */
+		void SetReadHandler(KLVObjectReadHandler Handler);
+
+		//! Get the length of the value field
+		Length GetLength(void);
 
 		//! Get a reference to the data chunk
 		DataChunkPtr& GetData(void);
