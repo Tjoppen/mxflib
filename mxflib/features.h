6f9c1f225c829df5f82347e66042a0c1ad33e3f7
matt-beard <matt-beard>
2005-10-09 13:39:31 +0000

    re-correct bracketing

--- mxflib/features.h
+++ mxflib/features.h
@@ -1,7 +1,7 @@
 /*! \file	features.h
  *	\brief	Control compile-time and run-time selectable library features
  *
- *	\version $Id: features.h,v 1.2 2005/10/09 13:14:24 matt-beard Exp $
+ *	\version $Id: features.h,v 1.3 2005/10/09 13:39:31 matt-beard Exp $
  *
  *  \detail 
  *  Library feature selection allows the behaviour of the library to be modified at run-time or compile-time.
@@ -55,17 +55,17 @@
 
 // Those features that may be enabled
 #ifndef MXFLIB_FEATURE_MASK
-#define MXFLIB_FEATURE_MASK (-1)
+#define MXFLIB_FEATURE_MASK (UINT64_C(-1))
 #endif
 
 // Those features that are enabled by default
 #ifndef MXFLIB_FEATURE_DEFAULT
-#define MXFLIB_FEATURE_DEFAULT (0)
+#define MXFLIB_FEATURE_DEFAULT (UINT64_C(0))
 #endif
 
 // Those features that cannot be changed at run-time
 #ifndef MXFLIB_FEATURE_LOCK
-#define MXFLIB_FEATURE_LOCK (0)
+#define MXFLIB_FEATURE_LOCK (UINT64_C(0))
 #endif
 
 
@@ -73,15 +73,13 @@ namespace mxflib
 {
 	/* Standard library features (bits 0 to 30) */
 
-	const UInt64 Feature0 = UINT64_C(1 << 0);			//!< MXFLib feature: Not used
+	const UInt64 Feature0 = UINT64_C(1) << 0;			//!< MXFLib feature: Not used
 
 	// Reserve a bit for user-extensions
-	const UInt64 UserExtension = UINT64_C(1 << 31);		//!< MXFLib feature: Reserved to allow user extensions
+	const UInt64 UserExtension = UINT64_C(1) << 31;		//!< MXFLib feature: Reserved to allow user extensions
 
 
-	/* Non-Standard library functions - may cause non-complient behaviour (bits 32 to 63)
-	 * DRAGONS: Note the different positioning of the brackets in the UINT64_C macro for bits 32 onwards
-	 */
+	/* Non-Standard library functions - may cause non-complient behaviour (bits 32 to 63) */
 
 	const UInt64 Feature32 = UINT64_C(1) << 32;
 
