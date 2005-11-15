257fb556f262c8837adf94079bc91b96921558d7
matt-beard <matt-beard>
2005-11-15 12:29:00 +0000

    Add IsPictureEssence() etc. to EssenceSource

--- mxflib/essence.h
+++ mxflib/essence.h
@@ -1,7 +1,7 @@
 /*! \file	essence.h
  *	\brief	Definition of classes that handle essence reading and writing
  *
- *	\version $Id: essence.h,v 1.11 2005/10/27 11:05:00 matt-beard Exp $
+ *	\version $Id: essence.h,v 1.12 2005/11/15 12:29:00 matt-beard Exp $
  *
  */
 /*
@@ -224,6 +224,39 @@ namespace mxflib
 		 *        they will always return true from this function
 		 */
 		virtual bool GetNonGC(void) { return NonGC; }
+
+		/* Essence type identification */
+		/* These functions can be overwridden, or use the base versions to parse GetGCEssenceType() */
+
+		//! Is this picture essence?
+		virtual bool IsPictureEssence(void)
+		{
+			UInt8 Type = GetGCEssenceType();
+			if((Type == 0x05) || (Type == 0x15)) return true;
+			return false;
+		}
+
+		//! Is this sound essence?
+		virtual bool IsSoundEssence(void)
+		{
+			UInt8 Type = GetGCEssenceType();
+			if((Type == 0x06) || (Type == 0x16)) return true;
+			return false;
+		}
+
+		//! Is this data essence?
+		virtual bool IsDataEssence(void)
+		{
+			UInt8 Type = GetGCEssenceType();
+			if((Type == 0x07) || (Type == 0x17)) return true;
+			return false;
+		}
+
+		//! Is this compound essence?
+		virtual bool IsCompoundEssence(void)
+		{
+			return (GetGCEssenceType() == 0x18);
+		}
 	};
 
 	// Smart pointer to an EssenceSource object
