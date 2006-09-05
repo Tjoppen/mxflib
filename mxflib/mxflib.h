08a3af72d994d71b6d138a77c4a557f705835221
matt-beard <matt-beard>
2006-09-05 08:39:22 +0000

    Set version to 1.0.0(2)-Dev

--- mxflib/mxflib.h
+++ mxflib/mxflib.h
@@ -1,7 +1,7 @@
 /*! \file	mxflib.h
  *	\brief	The main MXFLib header file
  *
- *	\version $Id: mxflib.h,v 1.20 2006/09/03 12:52:35 matt-beard Exp $
+ *	\version $Id: mxflib.h,v 1.21 2006/09/05 08:39:22 matt-beard Exp $
  *
  */
 /*
@@ -50,7 +50,7 @@ namespace mxflib
 #define MXFLIB_VERSION_MAJOR "1"
 #define MXFLIB_VERSION_MINOR "0"
 #define MXFLIB_VERSION_TWEAK "0"
-#define MXFLIB_VERSION_BUILD "0"
+#define MXFLIB_VERSION_BUILD "2"
 #define MXFLIB_VERSION_REL   2
 #define MXFLIB_VERSION_RELTEXT(REL) (REL==1?"-Release":(REL==2?"-Development":(REL==3?"-Patched":(REL==4?"-Beta":(REL==5?"-Private":"")))))
 #define MXFLIB_VERSION_RELNUMBER(REL) (REL==1?"1":(REL==2?"2":(REL==3?"3":(REL==4?"4":(REL==5?"5":"0")))))
