2a1f1650e1423a2d803d3ece53ecef9bbe212839
matt-beard <matt-beard>
2007-01-19 14:39:16 +0000

    Add properties that will allow searching within the RIP

--- mxflib/rip.cpp
+++ mxflib/rip.cpp
@@ -4,7 +4,7 @@
  *			The RIP class holds Random Index Pack data, either loaded from a real
  *			Random Index Pack in the file or built by discovering partitions.
  *
- *	\version $Id: rip.cpp,v 1.3 2005/09/26 08:35:59 matt-beard Exp $
+ *	\version $Id: rip.cpp,v 1.4 2007/01/19 14:39:16 matt-beard Exp $
  *
  */
 /*
@@ -32,25 +32,27 @@
 
 #include <mxflib/mxflib.h>
 
+using namespace mxflib;
+
 
 //! Random Index Pack constructor
 /*! Sets the pack as generated (as it obviously hasn't been read yet)
 */
-mxflib::RIP::RIP()
+RIP::RIP()
 {
 	isGenerated = true;
 }
 
 
 //! Random Index Pack destructor
-mxflib::RIP::~RIP()
+RIP::~RIP()
 {
-	debug("~RIP()\n");
+	//debug("~RIP()\n");
 }
 
 
 //! Add a partition to a RIP
-void mxflib::RIP::AddPartition(PartitionPtr Part,
+PartitionInfoPtr RIP::AddPartition(PartitionPtr Part,
 						  Position Offset			/* = -1 */, 
 						  UInt32 SID				/* = 0 */)
 {
@@ -63,17 +65,236 @@ void mxflib::RIP::AddPartition(PartitionPtr Part,
 
 	// Add the new partition
 	insert(PartitionInfoMap::value_type(Offset, NewPI));
+
+	// Return the new item
+	return NewPI;
 }
 
 
 //! PartitionInfo constructor
-mxflib::PartitionInfo::PartitionInfo(PartitionPtr Part	/* = NULL */,
+PartitionInfo::PartitionInfo(PartitionPtr Part	/* = NULL */,
 									 Position Offset			/* = -1 */, 
 									 UInt32 SID					/* = 0 */)
-	: ThePartition(Part), ByteOffset(Offset), BodySID(SID)
+	: ThePartition(Part), ByteOffset(Offset), BodySID(SID),
+	  IndexSID(0), KnownSIDs(false), StreamOffset(-1), StreamByteCount(-1), EstStreamOffset(-1), EstStreamByteCount(-1), EssenceStart(-1)
 {
 	debug("Generating new PartitionInfo\n");
 }
 
 
+//! Locate the previous partition to a given location
+/*! Finds the nearest partition that is located before a given location
+ *  \return Smart pointer to the PartitionInfo block, or NULL if no entries exist before the specified position
+ */
+PartitionInfoPtr RIP::FindPreviousPartition(Position Pos)
+{
+	// Nothing to return if the RIP is empty
+	if(empty()) return NULL;
+
+	// Find the current partition at this location, or the nearest after it
+	RIP::iterator it = lower_bound(Pos);
+
+	// If the first entry was returned then there is no preceeding entry - again we return NULL
+	if(it == begin()) return NULL;
+
+	// Index the previous entry
+	it--;
+
+	return (*it).second;
+}
+
+
+//! Locate the next partition to a given location
+/*! Finds the nearest partition that is located after a given location
+ *  \return Smart pointer to the PartitionInfo block, or NULL if no entries exist after the specified position
+ */
+PartitionInfoPtr RIP::FindNextPartition(Position Pos)
+{
+	// Nothing to return if the RIP is empty
+	if(empty()) return NULL;
+
+	// Find the first partition after this location
+	RIP::iterator it = lower_bound(Pos + 1);
+
+	// If no entry returned then there is no known partition after this position - again we return NULL
+	if(it == end()) return NULL;
+
+	return (*it).second;
+}
+
+//! Locate the partition <b>most likely</b> to contain the given stream offset for a SID
+/*! This may or may not be the correct partition depending on what values are know about partitions,
+ *  so it is up to the caller to verify this.
+ *  \note If the RIP is incomplete you will get strange results
+ *  \return A pointer to the PartitionInfo block, or NULL if there was a problem
+ */
+PartitionInfoPtr RIP::FindPartition(UInt32 SID, Position StreamOffset)
+{
+	// *****************************************************
+	// DRAGONS: Simple algorithm, does not use any indexing
+	// *****************************************************
+
+	bool PositionAccurate = true;				//!< Cleared once we have made a guestimate of StreamPosition
+	Position StreamPosition = 0;				//!< The real, or guestimated, stream offset of the current partition
+
+	// Get the first partition
+	RIP::iterator it = begin();
+
+	// Details of the previous partition for this stream
+	RIP::iterator PrevPartition = end();		// The previous partition for this SID, or end() if we haven't found one
+	Position PrevStart = -1;					// The byte offset within the file of the previous partition, or -1 if not known
+	Position PrevEnd = -1;						// The byte offset within the file of the start of the following partition (of any stream), or -1 if not known
+
+	// The estimated size of the start of a partition (the partition pack and any other bits)
+	// We will update this as we learn more about the file
+	Length PartitionEstimate = 0;
+
+	// The KAG used for this stream - if known
+	Length StreamKAG = 0;
+
+	while(it != end())
+	{
+		// If this is the first partition following "PrevPartition" record this position as the end of the previous partition
+		if((PrevEnd == -1) && (PrevStart != -1)) PrevEnd = (*it).second->ByteOffset;
+
+		if((*it).second->BodySID == 0)
+		{
+			// Should we read the partition to check?
+		}
+
+		// This partition is "one of ours"
+		if((*it).second->BodySID == SID)
+		{
+			// We know the stream offset for this partition
+			if((*it).second->GetStreamOffset() != -1)
+			{
+				// Update the stream position with the known value
+				StreamPosition = (*it).second->GetStreamOffset();
+
+				// If we overshoot we must retrun the previous partition - this is done by exiting the loop
+				if(StreamPosition < StreamOffset) break;
+
+				// If we have not over-shot the desired position, we have now updated our stream position with an accurate value
+				PositionAccurate = true;
+			}
+			else
+			{
+				/* If we have a copy of the partition pack we can read the value from there (and other items used for later estimates) */
+				if((*it).second->ThePartition)
+				{
+					/* We have a copy of the partition pack - this is going well */
+
+					// Read the KAG size for this stream (should be constant through the stream)
+					StreamKAG = (*it).second->ThePartition->GetInt64("KAGSize");
+
+					// Estimate the size of the header metadata and index table segments
+					PartitionEstimate = (*it).second->ThePartition->GetInt64("HeaderByteCount");
+					PartitionEstimate += (*it).second->ThePartition->GetInt64("IndexByteCount");
+
+					// Add in the size of this partition pack
+					DataChunkPtr Pack = (*it).second->ThePartition->WriteObject();
+					PartitionEstimate += Pack->Size;
+
+					// Read the actual offset
+					StreamPosition = (*it).second->ThePartition->GetInt64("BodyOffset");
+					(*it).second->SetEstimatedStreamOffset(StreamPosition);
+					PositionAccurate = true;
+				}
+				else
+				{
+					/* We must estimate a value */
+
+					// If the last position was unknown then we must assume that this is the first partition for this stream
+					if(PrevPartition == end())
+					{
+						// This still counts as an estimate!
+						StreamPosition = 0;
+						PositionAccurate = false;
+					}
+					else
+					{
+						if((PrevStart == -1) || (PrevEnd == -1))
+						{
+							// We have a problem as there was a previous partition for this stream, but it was not complete!
+							// All we can really do is flag that we have an estimated value!
+							PositionAccurate = false;
+						}
+						else
+						{
+							/* We know the start and end of the previous partition, so start estimating */
+
+							// Use the previously calculated estimate (if we have one)
+							if((*it).second->GetEstimatedStreamOffset())
+							{
+								StreamPosition = (*it).second->GetEstimatedStreamOffset();
+								PositionAccurate = false;
+
+								// If we overshoot we must retrun the previous partition - this is done by exiting the loop
+								if(StreamPosition < StreamOffset) break;
+							}
+							else
+							{
+								// Start working out where stream data starts
+								Position DataOffset = PartitionEstimate;
+
+								// Round to the next KAG
+								if(StreamKAG > 1)
+								{
+									// Calculate the number of bytes we are from the current KAG
+									Position Align = DataOffset % StreamKAG;
+									
+									// Only align if not already aligned
+									if(Align != 0)
+									{
+										// Work out the number of bytes to the next KAG
+										Align = StreamKAG - Align;
+
+										// We need to add extra if we can't fit a filler in - for small KAGs we may need to add several
+										while(Align < 17) Align += StreamKAG;
+								
+										// Add the filler
+										DataOffset += Align;
+									}
+								}
+
+								// Add in the size of essence in the previous partition
+								StreamPosition += (PrevEnd - (PrevStart + DataOffset));
+								PositionAccurate = false;
+
+								// Set this new estimated position (for later use)
+								(*it).second->SetEstimatedStreamOffset(StreamPosition);
+							}
+
+							// If we overshoot we must retrun the previous partition - this is done by exiting the loop
+							if(StreamPosition < StreamOffset) break;
+
+							/* We should assume that only the header has header metadata, unless we have found some elsewhere,
+							* so if we calculated PartitionEstimate from the header, remove the header byte count */
+							if(((*it).first == 0) && ((*it).second->ThePartition))
+							{
+								PartitionEstimate -= (*PrevPartition).second->ThePartition->GetInt64("IndexByteCount");
+							}
+						} // End know size of previous partition
+					} // End not the first partition of this stream
+				} // End estimation block
+			}
+
+			// Record this as the previous one of our partitions
+			PrevStart = (*it).second->ByteOffset;
+			PrevPartition = it;
+		}
+	
+		// Move on to the next partition
+		it++;
+	}
+
+	// Return the last partition found for this stream, this will be the "previous partition" if we over-shot,
+	// or the last partition if this stream if we ran off the end of the RIP
+
+	// If we didn't find any partitions for this stream, return an error status
+	if(PrevPartition == end()) return NULL;
+
+	// Return a pointer to the PartitionInfo structure
+	return (*PrevPartition).second;
+}
 
--- mxflib/rip.h
+++ mxflib/rip.h
@@ -4,7 +4,7 @@
  *			The RIP class holds Random Index Pack data, either loaded from a real
  *			Random Index Pack in the file or built by discovering partitions.
  *
- *	\version $Id: rip.h,v 1.3 2005/09/26 08:35:59 matt-beard Exp $
+ *	\version $Id: rip.h,v 1.4 2007/01/19 14:39:16 matt-beard Exp $
  *
  */
 /*
@@ -44,9 +44,13 @@ namespace mxflib
 	{
 	public:
 		PartitionPtr ThePartition;	//!< The actual partition
+									/*!< \note This is public for compatibility only <b>** use accessors **</b>
+									 */
 
 		Position ByteOffset;		//!< Byte offset into the file for the start of this partition
-									/*!< \note
+									/*!< \note This is public for compatibility only <b>** use accessors **</b>
+									 *
+									 *   \note
 									 *	 Version 11 of the MXF spec uses a UInt64 for this
 									 *         field but we are using a Position type here as it
 									 *         makes more sense, and allows distingushed value -1
@@ -57,15 +61,99 @@ namespace mxflib
 									 */
 
 		UInt32 BodySID;				//!< Stream ID of any essence in this partition (0 if none)
-									/*!< \note 0 is also used if the existance of essence is
+									/*!< \note This is public for compatibility only <b>** use accessors **</b>
+									 *
+									 *   \note 0 is also used if the existance of essence is
 									 *         has not yet been determined
 									 */
+
+	protected:
+		UInt32 IndexSID;			//!< Index SID of any index table in this partition (0 if none or not known)
+
+		bool KnownSIDs;				//!< Set true once we know for sure what the SIDs are, including IndexSID
+									/*!< This will be false when we have read a version 1 RIP as all that is known is the SID,
+									 *   which could possibly be an index SID for index-only partitions, but will be true
+									 *   once we have either parsed the partition pack itself, written one ourselves, or
+									 *   read a version 2 RIP (complete with detailed partion layout)
+									 */
+
+		Position StreamOffset;		//!< Stream offset of the first byte of essence, or generic stream, data in the partition
+									/*!< Set to -1 if not known
+									 */
+		Length StreamByteCount;		//!< Count of stream data bytes in the partition
+									/*!< Set to -1 if not known
+									 */
+
+		Position EstStreamOffset;	//!< <b>Estimated</b> Stream offset of the first byte of essence, or generic stream, data in the partition
+									/*!< Set to -1 if not known
+									 */
+		Length EstStreamByteCount;	//!< <b>Estimated</b> Count of stream data bytes in the partition
+									/*!< Set to -1 if not known
+									 */
+
+		Position EssenceStart;		//!< Actual byte offset in the file where the essence starts for this partition, if known, else -1
 
 	public:
 		PartitionInfo(PartitionPtr Part = NULL, Position Offset = -1, UInt32 SID = 0);
 
 		//! Comparison function to allow sorting
 		bool operator< (PartitionInfo &Other) {	return (ByteOffset < Other.ByteOffset); }
+
+		//! Get a pointer to the actual partition
+		PartitionPtr &GetPartition(void) { return ThePartition; }
+
+		//! Set the actual partition
+		void SetPartition(PartitionPtr Val) { ThePartition = Val; }
+
+		//! Get the BodySID
+		UInt32 GetBodySID(void) const { return BodySID; }
+
+		//! Set the BodySID
+		void SetBodySID(UInt32 Val) { BodySID = Val; }
+		
+		//! Get the IndexSID
+		UInt32 GetIndexSID(void) const { return IndexSID; }
+
+		//! Set the IndexSID
+		void SetIndexSID(UInt32 Val) { IndexSID = Val; }
+
+		//! Get KnownSIDs flag
+		bool SIDsKnown(void) const { return KnownSIDs; }
+
+		//! Set KnownSIDs flag
+		void SetKnownSIDs(bool Val) { KnownSIDs = Val; }
+
+		//! Set BodySID and IndexSID, and set KnownSIDs to true
+		void SetSIDs(UInt32 NewBodySID, UInt32 NewIndexSID)
+		{
+			BodySID = NewBodySID;
+			IndexSID = NewIndexSID;
+			KnownSIDs = true;
+		}
+
+		//! Get the byte offset of this partition pack in the file (if known), or -1 if not known
+		Position GetByteOffset(void) const { return ByteOffset; }
+
+		//! Set the byte offset of this partition pack in the file (if known), or -1 if not known
+		void SetByteOffset(Position Val)  { ByteOffset = Val; }
+
+		//! Get the stream offset of the first data byte in this partition (if known), or -1 if not known
+		Position GetStreamOffset(void) const { return StreamOffset; }
+
+		//! Set the stream offset of the first data byte in this partition (if known), or -1 if not known
+		void SetStreamOffset(Position Val)  { StreamOffset = Val; }
+
+		//! Get the <b>estimated</b> stream offset of the first data byte in this partition (if known), or -1 if no idea
+		Position GetEstimatedStreamOffset(void) const { return EstStreamOffset; }
+
+		//! Set the <b>estimated</b> stream offset of the first data byte in this partition (if known), or -1 if no idea
+		void SetEstimatedStreamOffset(Position Val)  { EstStreamOffset = Val; }
+
+		//! Get the essence start as a byte offset in the file (if known), or -1 if not known
+		Position GetEssenceStart(void) const { return EssenceStart; }
+
+		//! Set the essence start as a byte offset in the file (if known), or -1 if not known
+		void SetEssenceStart(Position Val)  { EssenceStart = Val; }
 	};
 
 	//! A smart pointer to a PartitionInfo object
@@ -87,13 +175,33 @@ namespace mxflib
 	public:
 		// DRAGONS: This should probably mutate into a "MatchedPysical" property
 		bool isGenerated;				//!< If not generated then the RIP has been read from a file
+
 	public:
 		RIP();
 		~RIP();
 
-		void AddPartition(PartitionPtr Part, Position Offset = -1, UInt32 SID = 0);
+		//! Add a partition at the specified location
+		/*! \note Replaces any existing entry at the same location
+		 */
+		PartitionInfoPtr AddPartition(PartitionPtr Part, Position Offset = -1, UInt32 SID = 0);
+
+		//! Locate the previous partition to a given location
+		/*! Finds the nearest partition that is located before a given location
+		 */
+		PartitionInfoPtr FindPreviousPartition(Position Pos);
+
+		//! Locate the next partition to a given location
+		/*! Finds the nearest partition that is located after a given location
+		 */
+		PartitionInfoPtr FindNextPartition(Position Pos);
+
+		//! Locate the partition <b>most likely</b> to contain the given stream offset for a SID
+		/*! This may or may not be the correct partition depending on what values are know about partitions,
+		 *  so it is up to the caller to verify this.
+		 *  \note If the RIP is incomplete you will get strange results
+		 */
+		PartitionInfoPtr FindPartition(UInt32 SID, Position StreamOffset);
 	};
 }
-
 
 #endif // MXFLIB__RIP_H
